fb65a215ff39e6aec7dd5dd890d4c9b649d8c648
HuamanAngel <oxipusioxx12@gmail.com>
2022-03-09 16:40:14 -0500

    Contectado con DB

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -10,19 +10,49 @@ namespace Blog.Controllers
     {
         public ActionResult Index()
         {
+            ViewBag.MyMessage = " Hi guys, how area you there.";
+            ViewBag.Vaca = "Mascarin";
+
             return View();
         }
 
-        public ActionResult About()
+        [HttpPost]
+        public ActionResult About(string email)
         {
-            ViewBag.Message = "Your application description page.";
-            return View();
-        }
 
+            List<Usuario> usuarios = new List<Usuario>(){
+                new Usuario{
+                    email = email,
+                    nombre = "Pedro castillo",
+                },
+                new Usuario{
+                    email = email,
+                    nombre = "Saco largo"
+                }
+            };
+            // ViewBag : no es recomendable al pasar dato de un modelo
+            // Loa adecuado es enviar el objeto
+
+            ViewBag.Message = "Your application description page.";
+            return View(usuarios);
+        }
+        [HttpGet]
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
             return View();
+        }
+    }
+
+
+    public class Usuario
+    {
+        public String email{get;set;}
+        public String nombre{get;set;}
+
+        public void SendEmail()
+        {
+
         }
     }
 }
